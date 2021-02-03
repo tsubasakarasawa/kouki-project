6a60a6d98cb1dfea4812379afeb30c139df2206e
tsubasakarasawa <zm19092@josai.ac.jp>
2021-02-03 00:45:51 +0000

    0203

---
+++ ex06-10.js
@@ -0,0 +1,8 @@
+function imgchange01() {
+    document.getElementById("logo01").src=
+     "./image/とんかつ.jpg";
+    }
+    function imgchange02() {
+    document.getElementById("logo01").src=
+     "./image/牛タン.jpg";
+    }
